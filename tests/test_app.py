5339408a34e12c1778a735e033294666b41376f3
Giovanna Manfredo <giovannamanfredo.dev@gmail.com>
2026-03-20 17:17:47 -0300

    added crud routes with simulated list

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,43 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_from_scratch.app import app
+def test_create_user(client):
+    response = client.post(
+        "/users/",
+        json={
+            "username": "alice",
+            "email": "alice@example.com",
+            "password": "secret",
+        },
+    )
 
-client = TestClient(app)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
 
 
-def test_read_root_should_return_ok_and_hello_world():
-    client = TestClient(app)
-    response = client.get("/")
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"message": "Olá Mundo!"}
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
