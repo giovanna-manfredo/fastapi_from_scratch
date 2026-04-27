931cc3ea5920c5e02cf204aa2f40f5553ee34ada
Giovanna Manfredo <giovannamanfredo.dev@gmail.com>
2026-04-27 20:08:57 -0300

    Connect Alembic with SQLAlchemy and created fist version of user's table

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from src.schemas import User, UserPublic
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World!"}
+@app.get("/users/", status_code=HTTPStatus.OK, response_class=list[UserPublic])
+def list_users():
+    ...
+
+@app.get("/users/{user_id}", status_code=HTTPStatus.OK)
+def get_user(user_id: int):
+    ...
+
+@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: User):
+    ...
+
